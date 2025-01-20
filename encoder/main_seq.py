1210ab26a9c01ec189f5c10fd5bb151d9f1d62b2
Aman <aman.swar001@gmail.com>
2025-01-20 20:10:30 +0530

    made the whole process sequential:

---
+++ encoder/main_seq.py
@@ -0,0 +1,161 @@
+import os
+import psutil
+from multiprocessing import Process , Queue , Manager
+import faiss
+import numpy as np
+import traceback 
+import argparse
+import torch
+from tqdm import tqdm
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+import encoder.config as config
+import encoder.utils as utils
+import encoder.embedding as embedding
+
+parser = argparse.ArgumentParser(description="arg parser for cli")
+parser.add_argument("--verbose" , action="store_true" , help="too see which directory its currently at")
+args = parser.parse_args()
+
+
+search_dir = "/home/aman/code/searchsp/test/tempsearchdir"
+content_extractor_func = {
+    "pdf" : utils.pdf_extractor,
+    "txt" : utils.text_extractor,
+    "docx" : utils.docs_extractor,
+    "ppt" : utils.ppt_extractor,
+    "excel" : utils.excel_extractor,
+    "md" : utils.markdown_extractor
+}
+
+INDEX = faiss.IndexFlatL2(512)
+METADATA_MAP = {}
+
+def test_traversal():
+    try:
+        for dirpath , dirnames , filenames  in tqdm(os.walk(search_dir)):
+            for filename in filenames:
+                file_path = os.path.join(dirpath , filename)
+
+                #verbose
+                if args.verbose:
+                    print(f"Traversing directory : {dirpath}")
+                    print(f"current file : {filename}")
+
+                file_ext = file_path.split('.')[-1]
+                if file_ext in config.SUPPORTED_EXT_IMG or file_ext in config.SUPPORTED_EXT_TEXT:
+                    content_extract(file_path=file_path)
+
+    except Exception as e:
+        print(f"error in test traversal : {e}")
+        traceback.print_exc()
+
+
+def content_extract(file_path):
+    """
+    Function to extract contents from the files in queue
+
+    """
+    try:
+        #verbose
+        if args.verbose:
+            print(f"current file getting extracted : {file_path}")
+
+        file_meta_data = utils.get_meta(file_path=file_path)
+        file_ext = file_path.split('.')[-1]
+        # if text based files
+        if file_ext in config.SUPPORTED_EXT_TEXT:
+            
+            content = content_extractor_func[file_ext](file_path=file_path)
+            content_dic = {"content" : content , "metadata" : file_meta_data}
+            # CONTENT_QUEUE.put(content_dic)
+            generate_embedding(content_dic)
+
+            if args.verbose:
+                print(f"extracted file {file_path}")
+
+
+        #if image
+        if file_ext in config.SUPPORTED_EXT_IMG:
+            content_dic = {"content" : file_path + "~" , "metadata" : file_meta_data}
+            generate_embedding(content_dic)
+            if args.verbose:
+                print(f"Successfully processed img {file_path}")
+
+            
+    
+    except Exception as e:
+        print(f"error in content extraction phase : {e}")
+        traceback.print_exc()
+
+
+def generate_embedding(content_data):
+    """
+    Generates embedding of contents and store it into FIASS db
+    
+    """
+    try:  
+        content = content_data["content"]
+        metadata = content_data["metadata"]
+        generated_embedding = None
+
+        #handle the case of images
+        if content[-1] == "~":
+            img_content = content[:-1]
+            #double check to avoid edge cases
+            if os.path.exists(img_content):
+                '''handel images''' 
+                generated_embedding = embedding.image_extract(img_content)
+
+        else:
+            '''handle texts'''
+            generated_embedding = embedding.text_extract(content)
+
+        if generated_embedding is not None:
+            #ensuring its a numpy array
+            if isinstance(generated_embedding ,torch.Tensor):
+                generated_embedding = generated_embedding.cpu().numpy()
+            data = (generate_embedding , metadata)
+            store_embedding(data)
+            
+    except Exception as e:
+        print(f"error while generating and storing embedding : {e}")
+        traceback.print_exc()
+
+
+
+def store_embedding(data:tuple):
+    """
+    Function to add the embedding and metadata 
+    Args:
+        embedding (torch.tensor) : embedding vector of any content
+        metadata (dict) : metadata regarding the file
+    """
+
+    try:
+        embedding_vec , metadata = data
+
+        if embedding_vec.ndim == 1:
+            embedding_np = embedding_vec.reshape(1 , -1)
+        else:
+            embedding_np = embedding_vec
+
+        embedding_np = embedding_np.astype('float32')
+        faiss_id = INDEX.ntotal
+        INDEX.add(embedding_np)
+        METADATA_MAP[faiss_id] = metadata
+
+
+        if args.verbose:
+            print(f"Added Embedding {faiss_id} to index")
+
+    except Exception as e:
+        print(f"Error which storing embedding : {e}")
+        traceback.print_exc()
+
+
+if __name__ == "__main__":
+
+    test_traversal()
