959b4ba58305c60855176e3360cec3293e154663
Aman <aman.swar001@gmail.com>
2025-01-25 02:21:25 +0530

    add progress_bar function

--- query/utils.py
+++ query/utils.py
@@ -1,8 +1,10 @@
 from transformers import AutoTokenizer , MobileBertForSequenceClassification
 import torch
+from tqdm import tqdm
+import threading
+import time
 
-
-save_directory = "/query/results/saved_model"
+save_directory = "query/results/saved_model"
 # Load the saved model and tokenizer
 model = MobileBertForSequenceClassification.from_pretrained(save_directory)
 tokenizer = AutoTokenizer.from_pretrained(save_directory)
@@ -13,6 +15,12 @@ labeltoid = {0 : "TEXT" , 1 : "IMAGE"}
 def index_token(query:str):
     """
     Query function which utilize mobilebert for generating token
+
+    args:
+        query (str) => user query in normal format
+    
+    returns:
+        predicted token -> TEXT / IMAGE
     """
 
     inp = tokenizer(text=query,
@@ -32,7 +40,38 @@ def index_token(query:str):
     return labeltoid[predicted_label]
 
 
+def progress_bar(func , *args , **kwargs):
+
+    stop_event = threading.Event()
+
+    def bar():
+        with tqdm(total=100 , unit="%" , ncols=100 , desc="Searching ...") as pbar:
+            while not stop_event.is_set():
+                for _ in range(10):
+                    time.sleep(0.1)
+                    pbar.update(1)
+
+                    if stop_event.is_set():
+                        break
+
+            pbar.n = pbar.total
+            pbar.close()
+
+
+    thread = threading.Thread(target=bar)
+    thread.start()
+
+    try:
+        result = func(*args , **kwargs)
+        return result
     
+    finally:
+        stop_event.set()
+        thread.join()
+
+
+    
+
 
     
 
