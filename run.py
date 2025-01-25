bb76f9918b23df03a87dadb61d3ad152108737b0
Aman <aman.swar001@gmail.com>
2025-01-25 18:11:31 +0530

    run this file to run

---
+++ run.py
@@ -0,0 +1,43 @@
+import encoder.main_seq
+import encoder.utils
+from query import query
+import encoder
+from encoder.main_seq import dir_traversal
+
+import os
+import time
+
+# init
+
+search_dir = ""
+
+
+while True:
+    if search_dir == "":
+        print("enter search directory : ")
+        temp_search_dir = input()
+
+        search_dir = encoder.utils.prep_dir(temp_search_dir)
+
+        if not os.path.exists(search_dir):
+            print(search_dir)
+            print("invalid please try again")
+
+        else:
+            break
+
+
+print("GENERATING EMBEDDING")
+start_time = time.time()
+dir_traversal(search_dir=search_dir)
+end_time = time.time() - start_time
+
+print(f"Generated embeddings in {end_time:2f} secs")
+
+while True:
+    print("\nEnter query : ...")
+    q = input()
+
+    query.search(q)
+
+
